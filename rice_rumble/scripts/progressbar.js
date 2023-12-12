cf6f3617695f7275e348cb8cdf3cc664b7fd422b
Soh Jun Wen <121473009+jwsoh07@users.noreply.github.com>
2023-12-13 00:13:49 +0800

    Fix progress bar UI issues

--- rice_rumble/scripts/progressbar.js
+++ rice_rumble/scripts/progressbar.js
@@ -1,15 +1,13 @@
 class ProgressBar {
-
-
-    updatePercentage(level) {
-        var percentage = document.getElementById("percentage");
-        percentage.style.marginLeft = level + "%";
+    constructor() {
+        this.updateProgressBar(0);
     }
 
-    updateProgressBar(level, score) {
+    updateProgressBar(level, score = 0) {
         var progressLevel = document.getElementById("progressLevel");
-        progressLevel.style.width = level + 1 + "%";
-        this.updatePercentage(level);
+        // to offset the icon marker on the progress bar a little bit to the right
+        // so that it sits within the PB at the beginning.
+        progressLevel.style.width = level + 2 + "%";
 
         // doStarJump(score);
         if (level < 100) {
@@ -37,40 +35,7 @@ class ProgressBar {
 
 export default ProgressBar;
 
-// function updatePercentage(level) {
-//     var percentage = document.getElementById("percentage");
-//     percentage.style.marginLeft = level + "%";
-// }
 
-// function updateProgressBar(level, score) {
-
-//     var progressLevel = document.getElementById("progressLevel");
-//     progressLevel.style.width = level + 1 + "%";
-//     updatePercentage(level);
-
-//     // doStarJump(score);
-
-//     if (level < 100) {
-//         progressLevel.classList.remove("fullbar");
-//     } else {
-//         progressLevel.classList.add("fullbar");
-//     }
-// }
-
-// function doStarJump(score) {
-//     // we could set up a point system to display different stars indicating different points by using an argument and conditional statements to display different star elements.  
-
-//     // remove animations from all stars
-//     const stars = document.querySelectorAll('.star');
-//     stars.forEach(star => {
-//         star.classList.remove('star-jump');
-//     });
-
-//     // add the animation on the corresponding star for the given score
-//     setTimeout(() => {
-//         document.getElementById('star-' + score).classList.add('star-jump');
-//     }, 10);
-// }
 
 
 
