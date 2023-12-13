28fa8599446a7565701f39ec07cd9b33337043a3
Soh Jun Wen <121473009+jwsoh07@users.noreply.github.com>
2023-12-13 18:12:56 +0800

    Attach overlay background to modal

--- rice_rumble/scripts/modal.js
+++ rice_rumble/scripts/modal.js
@@ -1,17 +1,27 @@
 class Modal {
     constructor() {
         this.notEnoughRice = document.querySelector('.results.encourage');
-        this.overlay = null; // to be added
+        this.overlay = document.querySelector('.overlay');
+    }
+
+    hideOverlay() {
+        this.overlay.classList.add('not-displayed');
+    }
+
+    showOverlay() {
+        this.overlay.classList.remove('not-displayed');
     }
 
     showNotEnoughRice() {
         this.notEnoughRice.classList.remove('not-displayed');
+        this.showOverlay();
     }
 
     executeFuncAfterHidingNotEnoughRice(func) {
         this.notEnoughRice.addEventListener('click', () => {
             func();
             this.notEnoughRice.classList.add('not-displayed');
+            this.hideOverlay();
         });
     }
 }
